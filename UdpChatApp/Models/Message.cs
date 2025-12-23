905e154d283f7696738b6ec49787f86562be9a3c
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2025-12-23 21:50:12 +0500

    Создание класса Message

---
+++ UdpChatApp/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace UdpChatApp.Models
+{
+    public class Message : ObservableObject
+    {
+        private string _text;
+        private string _senderName;
+        private string _recipient;
+        private MessageType _type;
+        private DateTime _timestamp;
+    }
+}
