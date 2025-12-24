c9280c6516048a4547aa8cbec72c07090c9482e2
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2025-12-24 22:05:37 +0500

    Добавление в класс Message констроукторов

--- UdpChatApp/Models/Message.cs
+++ UdpChatApp/Models/Message.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Net.WebSockets;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
 
 namespace UdpChatApp.Models
 {
@@ -10,5 +10,25 @@ namespace UdpChatApp.Models
         private string _recipient;
         private MessageType _type;
         private DateTime _timestamp;
+
+        //Пустой конструктор
+        public Message()
+        {
+             _text = string.Empty;
+             _senderName = string.Empty;
+             _recipient = string.Empty;
+             _type = MessageType.Public;
+             _timestamp = DateTime.Now;
+        }
+        
+        // Конструктрор с параметрами
+        public Message(string text, string senderName, MessageType type = MessageType.Public, string recipient = "")
+        {
+            _text = text;
+            _senderName = senderName;
+            _recipient = recipient;
+            _type = type;
+            _timestamp = DateTime.Now;
+        }
     }
 }
