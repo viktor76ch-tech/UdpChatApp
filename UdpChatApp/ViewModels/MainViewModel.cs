47837595eae36952e7a7649a9e53234236c204e5
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2025-12-25 21:29:56 +0500

    Начал писать  класс MainViewModel

---
+++ UdpChatApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using UdpChatApp.Models;
+
+namespace UdpChatApp.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string _messageText;
+        private string _status;
+        private string _isConnected;
+
+        public ObservableCollection<Message> Messages { get; } = new ObservableCollection<Message>();
+    }
+}
